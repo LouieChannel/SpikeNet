29506adc7d17051222e6b609108d7e0aac4d1270
LouieChannel <antonmirko.ukr@gmail.com>
2019-05-28 08:21:09 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Percent = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выберите файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_ClickAsync);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox1.Location = new System.Drawing.Point(225, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(321, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Настройкая подключения:";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button2.Location = new System.Drawing.Point(554, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Title,
+            this.Time,
+            this.Percent});
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 3);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(692, 348);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.2985F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.7015F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 146F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(698, 431);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // Title
+            // 
+            this.Title.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Title.HeaderText = "Title";
+            this.Title.MinimumWidth = 6;
+            this.Title.Name = "Title";
+            // 
+            // Time
+            // 
+            this.Time.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Time.HeaderText = "Time";
+            this.Time.MinimumWidth = 6;
+            this.Time.Name = "Time";
+            // 
+            // Percent
+            // 
+            this.Percent.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Percent.HeaderText = "Percent";
+            this.Percent.MinimumWidth = 6;
+            this.Percent.Name = "Percent";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(100, 399);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 21);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Мультивыбор";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 431);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Нейронная сеть";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Percent;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        string filePath = string.Empty;
+        string pathToServer = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button1_ClickAsync(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "wav files (*.wav)|*.wav";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+
+                    var header = new WavHeader();
+                    // Размер заголовка
+                    var headerSize = Marshal.SizeOf(header);
+                    var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                    var buffer = new byte[headerSize];
+                    fileStream.Read(buffer, 0, headerSize);
+
+                    // Чтобы не считывать каждое значение заголовка по отдельности,
+                    // воспользуемся выделением unmanaged блока памяти
+                    var headerPtr = Marshal.AllocHGlobal(headerSize);
+                    // Копируем считанные байты из файла в выделенный блок памяти
+                    Marshal.Copy(buffer, 0, headerPtr, headerSize);
+                    // Преобразовываем указатель на блок памяти к нашей структуре
+                    Marshal.PtrToStructure(headerPtr, header);
+
+                    Console.WriteLine("Sample rate: {0}", header.SampleRate);
+                    Console.WriteLine("Channels: {0}", header.NumChannels);
+                    Console.WriteLine("Bits per sample: {0}", header.BitsPerSample);
+
+                    int sizeOfLayer = header.BitsPerSample * 1024;
+
+                    var array = File.ReadAllBytes(filePath);
+
+                    if (checkBox1.Checked == false)
+                    {
+                        int counter = 44;
+
+                        byte[] sounds;
+                        do
+                        {
+                            sounds = new byte[sizeOfLayer];
+                            sounds = array.Skip(counter).Take(sizeOfLayer).ToArray();
+                            SendWebReqestAsync(sounds);
+                            counter += sizeOfLayer;
+                        }
+                        while (counter < array.Length);
+                    }
+
+                    //var result = await GetAnswer(pathToServer);
+
+                    // Освобождаем выделенный блок памяти
+                    Marshal.FreeHGlobal(headerPtr);
+                }
+            }
+        }
+
+        private async void SendWebReqestAsync(byte[] byteArray)
+        {
+            try
+            {
+                ByteArrayContent byteContent = new ByteArrayContent(byteArray);
+                var client = new HttpClient();
+                client.MaxResponseContentBufferSize = 256000;
+                HttpResponseMessage reponse = await client.PostAsync(pathToServer, byteContent);
+
+                if (reponse.IsSuccessStatusCode == true)
+                    return;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+        private async Task<string> GetAnswer(string url)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url); 
+
+            if (response.IsSuccessStatusCode == true)
+            {
+                string res = await response.Content.ReadAsStringAsync();
+
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<string>(res);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        // Структура, описывающая заголовок WAV файла.
+        internal class WavHeader
+        {
+            // WAV-формат начинается с RIFF-заголовка:
+
+            // Содержит символы "RIFF" в ASCII кодировке
+            // (0x52494646 в big-endian представлении)
+            public UInt32 ChunkId;
+
+            // 36 + subchunk2Size, или более точно:
+            // 4 + (8 + subchunk1Size) + (8 + subchunk2Size)
+            // Это оставшийся размер цепочки, начиная с этой позиции.
+            // Иначе говоря, это размер файла - 8, то есть,
+            // исключены поля chunkId и chunkSize.
+            public UInt32 ChunkSize;
+
+            // Содержит символы "WAVE"
+            // (0x57415645 в big-endian представлении)
+            public UInt32 Format;
+
+            // Формат "WAVE" состоит из двух подцепочек: "fmt " и "data":
+            // Подцепочка "fmt " описывает формат звуковых данных:
+
+            // Содержит символы "fmt "
+            // (0x666d7420 в big-endian представлении)
+            public UInt32 Subchunk1Id;
+
+            // 16 для формата PCM.
+            // Это оставшийся размер подцепочки, начиная с этой позиции.
+            public UInt32 Subchunk1Size;
+
+            // Аудио формат, полный список можно получить здесь http://audiocoding.ru/wav_formats.txt
+            // Для PCM = 1 (то есть, Линейное квантование).
+            // Значения, отличающиеся от 1, обозначают некоторый формат сжатия.
+            public UInt16 AudioFormat;
+
+            // Количество каналов. Моно = 1, Стерео = 2 и т.д.
+            public UInt16 NumChannels;
+
+            // Частота дискретизации. 8000 Гц, 44100 Гц и т.д.
+            public UInt32 SampleRate;
+
+            // sampleRate * numChannels * bitsPerSample/8
+            public UInt32 ByteRate;
+
+            // numChannels * bitsPerSample/8
+            // Количество байт для одного сэмпла, включая все каналы.
+            public UInt16 BlockAlign;
+
+            // Так называемая "глубиная" или точность звучания. 8 бит, 16 бит и т.д.
+            public UInt16 BitsPerSample;
+
+            // Подцепочка "data" содержит аудио-данные и их размер.
+
+            // Содержит символы "data"
+            // (0x64617461 в big-endian представлении)
+            public UInt32 Subchunk2Id;
+
+            // numSamples * numChannels * bitsPerSample/8
+            // Количество байт в области данных.
+            public UInt32 Subchunk2Size;
+
+            // Далее следуют непосредственно Wav данные.
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            pathToServer = textBox1.Text;
+        }
+    }
+}
